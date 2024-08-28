da5b1faf3f38a5f7ac7af574829c18f1f59a193e
Habibie <fahrudinhabibi4@gmail.com>
2024-08-28 10:07:43 +0700

    menambah section skills,education dan footer

--- javascript.js
+++ javascript.js
@@ -4,3 +4,12 @@
     menuIcon.addEventListener('click', () => {
         navMenu.classList.toggle('open');
     });
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
